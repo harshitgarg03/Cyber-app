114862e7263a82a2e98015cd836a0932224a1730
harshitgarg03 <47687970+harshitgarg03@users.noreply.github.com>
2021-11-15 22:19:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,139 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useEffect, useCallback, useContext } from "react";
+import Router from "next/router";
+import { useSelector, useDispatch } from "react-redux";
+import allActions from "../redux/actions";
+import GoogleLogin from "react-google-login";
+import { Card, Form, Input, Button, Divider, Typography } from "antd";
+import { API_BASE_URL } from "../constants/apiConstants";
+import axios from "axios";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const { Link } = Typography;
 
-  handleClick = api => e => {
-    e.preventDefault()
+function Login() {
+  const dispatch = useDispatch();
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  const responseGoogle = (response) => {
+    console.log(response);
+    axios
+      .post("https://www.api.accessapp.io/users/auth/google/", {
+        access_token: response.accessToken, // note the differences in key and value variable names
+        id_token: response.tokenId,
+      })
+      .then((res) => {
+        localStorage.setItem("userInfo", JSON.stringify(res.data));
+        dispatch(allActions.userActions.setUser(res.data));
+        Router.push("/");
+      });
+  };
 
-  render() {
-    const { loading, msg } = this.state
+  const onFinish = (values) => {
+    // console.log('Received values of form: ', values);
+    axios
+      .post(`${API_BASE_URL}/users/auth/login/`, values)
+      .then((res) => {
+        localStorage.setItem("userInfo", JSON.stringify(res.data));
+        dispatch(allActions.userActions.setUser(res.data));
+        Router.push("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+  return (
+    <>
+      <div style={{ paddingTop: 60 }}>
+        <Link href="/" passHref>
+          <h3 style={{ textAlign: "center" }}>Access</h3>
+        </Link>
+        <Card style={{ width: 540, margin: "auto" }}>
+          {/* <div style={{ margin: "auto", width: "100%" }}>
+            <GoogleLogin
+              clientId="57740025555-32jjlcubuplvsu89tfknbtnj0apr2qid.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              render={(renderProps) => (
+                <Button
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  variant="outlined"
+                  style={{
+                    width: "100%",
+                    textTransform: "none",
+                    background: "white",
+                    height: "40px",
+                    marginBottom: 10,
+                  }}
+                >
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBRF__vDRer9N3lzmW-FJTnaiCi1Vd7TvcHrdcjzU28RHD2kcpRVdZIQhLvZaksbBPpak&usqp=CAU"
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      marginRight: "20px",
+                    }}
+                    alt="google"
+                  />
+                  Log in with Google
+                </Button>
+              )}
+            />
+          </div>
+          <Divider plain>or</Divider> */}
+          <Form
+            name="basic"
+            requiredMark={false}
+            size="large"
+            autoComplete="off"
+            layout="vertical"
+            onFinish={onFinish}
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please enter your email" }]}
+            >
+              <Input />
+            </Form.Item>
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please enter your password" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <p style={{ margin: 0, marginTop: 5 }}>
+              <Link style={{ textAlign: "left", marginTop: 20 }}>
+                Forgot password?
+              </Link>
+            </p>
+            <Form.Item>
+              <Button
+                block
+                // type="primary"
+                htmlType="submit"
+                style={{
+                  marginTop: 24,
+                  marginBottom: 24,
+                  backgroundColor: "#00a690",
+                  border: "none",
+                  color: "white",
+                }}
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <span>
+            Dont have an account? <Link href='/register'>Sign up</Link>
+          </span>
+        </Card>
       </div>
-    )
-  }
+    </>
+  );
 }
 
-export default App
+export default Login;
